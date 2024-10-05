a5f8fa6bddd6c35b443d1ba54d4a08a5a018fa3e
judajaavAlt <132619919+judajaavAlt@users.noreply.github.com>
2024-10-04 20:13:55 -0500

    solucion ejercicios

--- work/Labs/Lab1/Exercises/exercise1.c
+++ work/Labs/Lab1/Exercises/exercise1.c
@@ -4,7 +4,15 @@
 int main() {
     #pragma omp parallel num_threads(10)
     {
-        printf("Hello from thread %d\n", omp_get_thread_num());
+        int thread_num = omp_get_thread_num();
+        if (thread_num % 2 == 0)
+        {
+            printf("Hello from thread %d, i'm an even thread\n", thread_num);
+        }
+        else
+        {
+            printf("Hello from thread %d, i'm an odd thread\n", thread_num);
+        }
     }
     return 0;
 }
--- work/Labs/Lab1/Exercises/exercise2.c
+++ work/Labs/Lab1/Exercises/exercise2.c
@@ -2,13 +2,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main() {
+    int i;
     int Array[100]; // Use rand() % 10000 for init
     int max = 0;
     int min = 100000; 
     int avg;
 
+    //rellena el array
+    #pragma omp parallel for num_threads(100)
+        for (i = 0; i < 100; i++)
+        {    
+        Array[i] = rand() % 10000;
+        }
+
+    //busca el maximo
+    #pragma omp parallel for reduction(max:max) num_threads(100)
+        for (i = 0; i < 100; i++)
+        {
+            max = Array[i];
+        }
+
+    //busca el minimo
+    #pragma omp parallel for reduction(min:min) num_threads(100)
+        for (i = 0; i < 100; i++)
+        {
+            min = Array[i];
+        }
+
+    //busca el promedio
+    #pragma omp parallel for reduction(+:avg) num_threads(100)
+        for (i = 0; i < 100; i++)
+        {
+            avg = Array[i];
+        }
+    avg /= 100;
+
+    //imprime el resultado
+    printf("Maximum: %d\n", max);
+    printf("Minimum: %d\n", min);
+    printf("Average: %d\n", avg);
 
     return 0;
 }
